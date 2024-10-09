fbefed0a8bff06016e871ee8c99b1e9c2d832056
edwardhumi <edward.humianto@gmail.com>
2024-10-09 23:15:22 +0800

    Add external LED control

--- src/main.c
+++ src/main.c
@@ -11,6 +11,10 @@
 #define LED_RED (18)
 #define LED_GREEN (19)
 #define LED_BLUE (1)
+// LED Pins
+#define FRONT_LED_START_PIN 0    // Start pin for front LED
+#define FRONT_LED_END_PIN 9      // End pin for front LED
+#define REAR_LED_PIN 10          // Pin for rear LED
 
 
 /* Delay routine */
@@ -47,6 +51,60 @@ void led_control(enum color_t color, uint8_t on) {
 		else
 			PTD->PDOR |= MASK(LED_BLUE);
 	}
+}
+
+/**** LED ****/
+void initLEDs() {
+	// Enable clock gating: turn on power for PORTC
+	SIM_SCGC5 |= SIM_SCGC5_PORTC_MASK;
+
+	// Configure PORTC pins for LED output
+	for (int i = FRONT_LED_START_PIN; i <= FRONT_LED_END_PIN; i++) {
+		PORTC->PCR[i] &= ~PORT_PCR_MUX_MASK;
+		PORTC->PCR[i] |= PORT_PCR_MUX(1); // GPIO
+		PTC->PDDR |= (1 << i); // Set pin as output
+		PTC->PDOR &= ~(1 << i);
+	}
+
+	// Configure rear LED pin
+	PORTC->PCR[REAR_LED_PIN] &= ~PORT_PCR_MUX_MASK;
+	PORTC->PCR[REAR_LED_PIN] |= PORT_PCR_MUX(1); // GPIO
+	PTC->PDDR |= (1 << REAR_LED_PIN); // Set pin as output
+}
+
+// Function to control front LEDs in running mode
+void controlFrontLED(int state) {
+	if(state == 0){
+		for (int i = FRONT_LED_START_PIN; i <= FRONT_LED_END_PIN; i++) {
+			PTC->PSOR |= (1 << i);
+		}
+	} else {
+		int currentLED = FRONT_LED_START_PIN + state - 1;
+		for (int i = FRONT_LED_START_PIN; i <= FRONT_LED_END_PIN; i++) {
+			if (i!=currentLED) {
+				PTC->PCOR |= (1 << i);
+			}
+			else {
+				PTC->PSOR |= (1 << currentLED);
+			}
+		}
+		delay(200000);
+	}
+}
+
+// Function to control rear LED
+void controlRearLED(int state) {
+	if (state) {
+		GPIOC->PSOR |= (1 << REAR_LED_PIN); // Turn on LED
+		delay(233000);
+		GPIOC->PCOR |= (1 << REAR_LED_PIN); // Turn off LED
+		delay(233000);
+	} else {
+		GPIOC->PSOR |= (1 << REAR_LED_PIN); // Turn on LED
+		delay(116500);
+		GPIOC->PCOR |= (1 << REAR_LED_PIN); // Turn off LED
+		delay(116500);
+	}      
 }
 
 void initGPIO() {
